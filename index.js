10c3e662fb1fddef326089f1f5a7358ffeee4d51
cdfsygh <felicitaspsg@hotmail.com>
2024-05-10 08:38:44 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const dropRightWhile = (arr, func) => {
-  while (arr.length > 0 && !func(arr[arr.length - 1])) arr = arr.slice(0, -1);
-  return arr;
-};
+function generate(numRows) {
+  const triangle = [];
+  for (let i = 0; i < numRows; i++) {
+    const row = new Array(i + 1).fill(1);
+    for (let j = 1; j < row.length - 1; j++) {
+      row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+    }
+    triangle.push(row);
+  }
+  return triangle;
+}
