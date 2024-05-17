43febf7afb8f17a6a448d36579d16fb781ba6293
cdfsygh <felicitaspsg@hotmail.com>
2024-05-17 20:15:55 +0000

    aptos task

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const dropRight = (arr, n = 1) => arr.slice(0, -n);
+function rob(nums) {
+  let prevMax = 0;
+  let currMax = 0;
+  for (const num of nums) {
+    const temp = currMax;
+    currMax = Math.max(prevMax + num, currMax);
+    prevMax = temp;
+  }
+  return currMax;
+}
